0bff103bb4c23156cfefbc80220a8c62ae6f99ce
loremercer <hugopochta@gmail.com>
2025-02-07 01:04:11 +0300

    added client for postgres

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
 	"net/http"
 	"quest_maker/handlers"
 )
 
 func main() {
+	dsn := "user=user password=password dbname=quest host=postgres port=5432 sslmode=disable"
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		panic(err)
+	}
+	db.SetMaxOpenConns(10)
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	rows, err := db.Query("SELECT 1")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(rows)
+
 	rootHandler := &handlers.RootHandler{}
 	http.Handle("/", rootHandler)
 	fmt.Println("starting server at :8080")
